33c6f1744dd9d540476e4926a400e510fc1a6490
Jeweloffice <jewel@influxiqtech.com>
2026-05-29 11:20:55 +0530

    push

--- server/server.js
+++ server/server.js
@@ -1,30 +1,33 @@
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
-const http = require('http');
 const cors = require("cors");
-const express = require("express")
 
 const app = express();
+
 app.use(cors());
 
-
 const server = http.createServer(app);
-const io = new Server(server,{cors:{
-    origin:"http://localhost:3000",
-    methods: ["GET","POST"],
-}})
 
-io.on("connection",(socket)=>{
-    console.log("User connected",socket.id)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
 
-    socket.on("send_message",(data)=>{
-        io.emit("receive_message",data)
-    })
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-    socket.on("disconnect",()=>{
-        console.log("User disconnected",socket.id)
-    })
-    //server listen
-    server.listen(5001,()=>{
-        console.log("server running")
-    })
-})
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
